e4577bcea05aa11780d29ae7037bacedc13a7d5d
Faeshal <faeshal111@gmail.com>
2020-07-11 12:34:47 +0700

    fetch api with axios

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,36 +1,15 @@
 import React, { Component } from "react";
 import UsertItem from "../users/UserItem";
+
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "2",
-        login: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        id: "3",
-        login: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
   render() {
     return (
       <div className="container">
-      <div className="row ">
-        {this.state.users.map((user) => (
-          <UsertItem key={user.id} user={user} />
-        ))}
-      </div>
+        <div className="row ">
+          {this.props.users.map((user) => (
+            <UsertItem key={user.id} user={user} />
+          ))}
+        </div>
       </div>
     );
   }
