02318d1834de568e0badec4463e7d4129e1b36e0
Faeshal <faeshal111@gmail.com>
2020-07-11 16:25:15 +0700

    add env variable and github api key

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import UsertItem from "../users/UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div className="container">
         <div className="row ">
-          {this.props.users.map((user) => (
+          {users.map((user) => (
             <UsertItem key={user.id} user={user} />
           ))}
         </div>
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 export default Users;
