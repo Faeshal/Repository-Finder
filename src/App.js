586b7e45faf4dbddf85c2c7e14732b1b3bf84df0
Faeshal <faeshal111@gmail.com>
2020-07-12 20:32:56 +0700

    implement context api in github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import GithubState from "./context/github/GithubState";
 import axios from "axios";
 
 const App = () => {
@@ -58,44 +59,46 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Alert alert={alert} />
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={(props) => (
-              <Fragment>
-                <Search
-                  searchUsers={searchUsers}
-                  clearUsers={clearUsers}
-                  showClear={users.length > 0 ? true : false}
-                  setAlert={showAlert}
+    <GithubState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Alert alert={alert} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={showAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path="/about" component={About} />
+            <Route
+              exact
+              path="/user/:login"
+              render={(props) => (
+                <User
+                  {...props}
+                  getUser={getUser}
+                  getUserRepos={getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
                 />
-                <Users loading={loading} users={users} />
-              </Fragment>
-            )}
-          />
-          <Route exact path="/about" component={About} />
-          <Route
-            exact
-            path="/user/:login"
-            render={(props) => (
-              <User
-                {...props}
-                getUser={getUser}
-                getUserRepos={getUserRepos}
-                user={user}
-                repos={repos}
-                loading={loading}
-              />
-            )}
-          />
-        </Switch>
-      </div>
-    </Router>
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </GithubState>
   );
 };
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = ({ icon, title }) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4 ">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           <i className={icon}></i>
           {title}
-        </a>
+        </Link>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
