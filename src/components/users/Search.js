3b029b5f4fc0ea2080c52f56e5bfdf93943b74be
Faeshal <faeshal111@gmail.com>
2020-07-12 22:17:59 +0700

    refactor to context api all

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Search = ({ clearUsers, showClear, setAlert, searchUsers }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
   const [text, setText] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (text === "") {
-      setAlert("Input Box Empty, Please Enter Something .....", "warning");
+      alertContext.setAlert(
+        "Input Box Empty, Please Enter Something .....",
+        "warning"
+      );
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText("");
     }
   };
@@ -37,20 +43,16 @@ const Search = ({ clearUsers, showClear, setAlert, searchUsers }) => {
           </div>
         </div>
       </form>
-      {showClear && (
-        <button className="btn btn-sm btn-dark mb-3" onClick={clearUsers}>
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-sm btn-dark mb-3"
+          onClick={githubContext.clearUsers}
+        >
           Clear
         </button>
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import UsertItem from "../users/UserItem";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
   if (loading) {
     return <Spinner />;
   } else {
@@ -17,11 +20,6 @@ const Users = ({ users, loading }) => {
       </div>
     );
   }
-};
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
 };
 
 export default Users;
