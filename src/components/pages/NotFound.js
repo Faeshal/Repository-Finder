82e20a061be153217919f763ba66bebf409d76be
Faeshal <faeshal111@gmail.com>
2020-07-12 22:32:36 +0700

    add notfound pages

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div className="text-center mt-4">
+      <h3>404 NOT FOUND</h3>
+    </div>
+  );
+};
+
+export default NotFound;
