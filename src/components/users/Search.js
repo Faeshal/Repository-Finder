abee26b412f38c881e932116f658527168bf8e45
Faeshal <faeshal111@gmail.com>
2020-07-12 11:38:06 +0700

    refactor search class component to functional component & implement useState

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,64 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  state = {
-    text: "",
-  };
+const Search = ({
+  showUsers,
+  clearUsers,
+  showClear,
+  setAlert,
+  searchUsers,
+}) => {
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert(
-        "Input Box Empty, Please Enter Something .....",
-        "warning"
-      );
+    if (text === "") {
+      setAlert("Input Box Empty, Please Enter Something .....", "warning");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      searchUsers(text);
+      setText("");
     }
   };
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setText(e.target.value);
   };
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div className="container">
-        <form onSubmit={this.onSubmit}>
-          <div className="input-group mb-4 mt-2">
-            <input
-              type="text"
-              name="text"
-              className="form-control"
-              placeholder="Type name here..."
-              value={this.state.text}
-              onChange={this.onChange}
-            />
-            <div className="input-group-append">
-              <button className="btn btn-dark" type="submit">
-                Search
-              </button>
-            </div>
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <div className="input-group mb-4 mt-2">
+          <input
+            type="text"
+            name="text"
+            className="form-control"
+            placeholder="Type name here..."
+            value={text}
+            onChange={onChange}
+          />
+          <div className="input-group-append">
+            <button className="btn btn-dark" type="submit">
+              Search
+            </button>
           </div>
-        </form>
-        {showClear && (
-          <button className="btn btn-sm btn-dark mb-3" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+        </div>
+      </form>
+      {showClear && (
+        <button className="btn btn-sm btn-dark mb-3" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
