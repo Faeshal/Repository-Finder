2bebc278e25dc415d4e46d0a37f1435e50d38393
Faeshal <faeshal111@gmail.com>
2020-07-17 09:52:13 +0700

    edit about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,25 @@
-import React, { Fragment } from "react";
+import React from "react";
+import Logo from "../layout/react.png";
 
 export default function About() {
   return (
-    <Fragment>
-      <h1>About Page</h1>
-      <h3>App for search github user, created with react</h3>
-    </Fragment>
+    <div class="row mx-auto">
+      <div className="col-md-12 text-center" style={col}>
+        <img src={Logo} style={{ width: "200px" }} />
+        <h2>
+          Created by Faeshal - With <b>React</b>
+        </h2>
+        <h4>Using Hooks & Context API</h4>
+        <h4>Docs: <a href="https://github.com/Faeshal/Repository-Finder">Github</a> </h4>
+      </div>
+    </div>
   );
 }
+
+const col = {
+  margin: "0",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+};
